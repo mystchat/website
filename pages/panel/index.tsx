6ab5bf026c2259ace7bae3cfc634f77c49853903
vKxni <Konradtoesch@icloud.com>
2022-05-28 10:11:18 +0200

    added subpage template

---
+++ pages/panel/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+
+// subpage where all the magic happens
+const Panel: NextPage = () => {
+    return (
+        <>
+        <h1>Panel</h1>
+        </>
+    )
+}
+
+export default Panel; 
